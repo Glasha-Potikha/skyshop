2b86f8427382358333cc89b67bc972ff5b6ffd60
Glasha <firatiha7404@gmail.com>
2025-01-12 19:00:39 +0300

    ДЗ.ООП-3

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -5,10 +5,59 @@ import org.skypro.skyshop.basket.Basket;
 import org.skypro.skyshop.product.SimpleProduct;
 import org.skypro.skyshop.product.DiscountedProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
+import org.skypro.skyshop.search.SearchEngine;
+import org.skypro.skyshop.article.Article;
+import org.skypro.skyshop.search.Searchable;
+
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
         testAppStationery();
+        System.out.println("________________" + '\n');
+        testAppStationeryWithSearchAndArticle();
+    }
+
+    public static void testAppStationeryWithSearchAndArticle() {
+        //товары канцелярского магазина
+        Searchable pen = new SimpleProduct("ручка синяя", 15);
+        Searchable pencil = new FixPriceProduct("карандаш");
+        Searchable album = new FixPriceProduct("альбом для рисования");
+        Searchable squaredNotebook = new DiscountedProduct("тетрадь в клетку", 12, 15);
+        Searchable linedNotebook = new DiscountedProduct("тетрадь в линейку", 11, 10);
+        SimpleProduct coloredCardboard = new SimpleProduct("цветной картон", 60);
+
+        SearchEngine stationery = new SearchEngine(9);
+
+        //статьи о товарах канцелярского магазина
+        Searchable aboutPen = new Article("Шариковой ручка", "Ша́риковая ру́чка — разновидность ручки (авторучки), при письме которой чернила переносятся из резервуара на бумагу вращающимся шариком.");
+        Searchable aboutPencil = new Article("Графитовый карандаш", "Деревянная палочка со стержнем из графита для письма, рисования, черчения.");
+        Searchable aboutNotebook = new Article("Школьная тетрадь", "тетрадь, предназначенная для письма школьниками в период обучения в общеобразовательных учебных учреждениях и учреждениях начального профессионального образования.");
+        Searchable aboutAlbum = new Article("Художественный альбом", "Книга, тетрадь в переплёте для стихов, рисунков. В нашем магазине есть разнообразные альбомы, скетчбуки простые и профессиональные на любой вкус!))");
+
+        //Функциональность Article:
+        System.out.println(aboutPen);
+        System.out.println(((Article)aboutPencil).getName() + '\n');
+        //Строковое представление через интерфейс:
+        System.out.println(coloredCardboard.getStringRepresentation());
+        System.out.println(aboutAlbum.getStringRepresentation() + '\n');
+        //Функциональность SearchEngine:
+        stationery.add(pen);
+        stationery.add(pencil);
+        stationery.add(album);
+        stationery.add(squaredNotebook);
+        stationery.add(linedNotebook);
+        stationery.add(coloredCardboard);
+        stationery.add(aboutPen);
+        stationery.add(aboutPencil);
+        stationery.add(aboutNotebook);
+        //переполнение:
+        stationery.add(aboutAlbum);
+        //поиск:
+        System.out.println('\n' + "Поиск по слову - тетрадь: " + '\n' + Arrays.toString(stationery.search("тетрадь")) + '\n');
+        System.out.println("Поиск по слову - альбом: " + '\n' + Arrays.toString(stationery.search("альбом")) + '\n');
+        System.out.println("Поиск по слову - карандаш: " + '\n' + Arrays.toString(stationery.search("карандаш")) + '\n');
+        System.out.println("Поиск по слову - ручка: " + '\n' + Arrays.toString(stationery.search("ручка")));
     }
 
     public static void testAppStationery() {
